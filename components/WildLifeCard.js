5d9f18f7311b4eaac6c5ee1813b59d10d99c7f91
bemarchant <bemarchant@proton.me>
2022-12-22 08:30:55 -0300

    feat(app): added a working share option for wildlifecard

--- components/WildLifeCard.js
+++ components/WildLifeCard.js
@@ -1,5 +1,7 @@
 import { useRef, useLayoutEffect } from "react";
-import { Text, Image, Dimensions, View, StyleSheet } from "react-native";
+import { Share, Text, Image, Dimensions, View, StyleSheet } from "react-native";
+import { captureRef } from "react-native-view-shot";
+
 import ScreenHeaderButton from "./icons/ScreenHeaderButton";
 import ConservationStatusBar from "./svg/ConservationStatusBar";
 import CientificNameText from "./CientificNameText";
@@ -7,41 +9,38 @@ import CommonNameText from "./CommonNameText";
 import { CLIMBING_ZONE, KINGDOM } from "../utils/constants";
 import { downLoadWildLifeData } from "../utils/inat";
 import { WILD_LIFE_DATA } from "../utils/constants";
-import { captureRef } from "react-native-view-shot";
-import { Share } from "react-native";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const saveToCameraRoll = () => {
-  return;
-};
 export const WildLifeCard = ({ navigation }) => {
+  const viewRef = useRef();
+
+  const shareDummyImage = async () => {
+    try {
+      const uri = await captureRef(viewRef, {
+        format: "png",
+        quality: 1,
+      });
+      await Share.share({ url: uri });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
         return (
           <ScreenHeaderButton
-            name="arrow-down-circle-outline"
+            name="share-outline"
             color="white"
-            onPress={saveToCameraRoll}
+            onPress={shareDummyImage}
           />
         );
       },
     });
   });
-  //   const viewRef = useRef();
-  //   const shareDummyImage = async () => {
-  //     try {
-  //       const uri = await captureRef(viewRef, {
-  //         format: "png",
-  //         quality: 0.7,
-  //       });
-  //       await Share.open({ url: uri });
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
 
   let query1 = downLoadWildLifeData(CLIMBING_ZONE.elmanzano, KINGDOM.animalia);
 
@@ -63,26 +62,27 @@ export const WildLifeCard = ({ navigation }) => {
     let image_uri = getPhotoImageUri(observation);
 
     return (
-      //   <View style={styles.rootView} ref={viewRef}>
-      <View style={styles.rootView}>
-        <View style={styles.imageContainer}>
-          <Image
-            style={{
-              borderRadius: 20,
-            }}
-            source={{
-              uri: image_uri,
-              width: windowWidth,
-              height: windowHeight,
-            }}
-          ></Image>
-          <View style={[styles.infoContainer]}>
-            <CommonNameText>{communName}</CommonNameText>
-            <CientificNameText>{cientificName}</CientificNameText>
-            <ConservationStatusBar consevationStatus={""} height={12} />
-            <Text style={styles.infoText}>{climbingZone}</Text>
-            <Text style={styles.infoText}>{userName}</Text>
-            <Text style={styles.infoText}>{date}</Text>
+      <View style={styles.rootView} ref={viewRef}>
+        <View style={styles.rootView}>
+          <View style={styles.imageContainer}>
+            <Image
+              style={{
+                borderRadius: 20,
+              }}
+              source={{
+                uri: image_uri,
+                width: windowWidth,
+                height: windowHeight,
+              }}
+            ></Image>
+            <View style={[styles.infoContainer]}>
+              <CommonNameText>{communName}</CommonNameText>
+              <CientificNameText>{cientificName}</CientificNameText>
+              <ConservationStatusBar consevationStatus={""} height={12} />
+              <Text style={styles.infoText}>{climbingZone}</Text>
+              <Text style={styles.infoText}>{userName}</Text>
+              <Text style={styles.infoText}>{date}</Text>
+            </View>
           </View>
         </View>
       </View>
